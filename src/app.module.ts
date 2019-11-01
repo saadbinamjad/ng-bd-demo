b91691d75a57ae67d548559731f933a45d4b672d
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2019-11-01 12:16:00 +0600

    added events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpeakersModule } from './speakers/speakers.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [SpeakersModule],
+  imports: [SpeakersModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
