487a00a39d746d910aae0eb0c3c9634bb2d61532
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2019-11-01 20:00:19 +0600

    added typeorm and event entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpeakersModule } from './speakers/speakers.module';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './events/entities/event.entity';
 
 @Module({
-  imports: [SpeakersModule, EventsModule],
+  imports: [SpeakersModule, EventsModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'ng-bd-demo',
+    entities: [Event],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './entities/event.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Event])],
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
 })
-export class EventsModule {}
+export class EventsModule { }
