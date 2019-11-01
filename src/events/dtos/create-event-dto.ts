3defff40a5ecd9a3c4a37f6b12467674db183b1a
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2019-11-01 22:18:11 +0600

    added validation pipe

--- src/events/dtos/create-event-dto.ts
+++ src/events/dtos/create-event-dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateEventDto {
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
     readonly description: string;
 }
--- src/events/dtos/update-event-dto.ts
+++ src/events/dtos/update-event-dto.ts
@@ -1,4 +1,12 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 export class UpdateEventDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
     readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
     readonly description: string;
 }
