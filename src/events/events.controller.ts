b9d6bca379c42360f1b6a6f6919d5c7476cb4902
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2019-11-01 12:29:17 +0600

    added crud endpoints for events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+    @Get()
+    index(): string {
+        return 'index';
+    }
+
+    @Get()
+    show(): string {
+        return 'show';
+    }
+
+    @Post()
+    store(): string {
+        return 'store';
+    }
+
+    @Patch()
+    update(): string {
+        return 'update';
+    }
+
+    @Delete()
+    delete(): string {
+        return 'delete';
+    }
+}
